6af6df366c05b1b157575e05cbf2f51d7f050d62
shashankmaurya0043-stack <shashankmaurya0043@gmail.com>
2026-05-08 18:06:24 +0530

    Add Contact component with email information

---
+++ artifacts/quiz/src/pages/Contact.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <div style={{ padding: "20px", color: "white" }}>
+      <h1>Contact Us</h1>
+      <p>Email: shashankmaurya0043@gmail.com</p>
+    </div>
+  );
+}
