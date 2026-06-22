b1953f8e3634d0d30584ee459c6cc74d003ea365
shashankmaurya0043-stack <shashankmaurya0043@gmail.com>
2026-06-22 21:13:23 +0530

    Delete artifacts/quiz/src/pages/blog/M4PYQ2024.tsx

--- artifacts/quiz/src/pages/blog/M4PYQ2024.tsx
+++
@@ -1,7 +0,0 @@
-export default function M4PYQ2024() {
-  return (
-    <div className="min-h-screen flex items-center justify-center text-white bg-[#0F172A]">
-      <h1 className="text-2xl font-bold">M4 PYQ 2025 Coming Soon...</h1>
-    </div>
-  );
-}
