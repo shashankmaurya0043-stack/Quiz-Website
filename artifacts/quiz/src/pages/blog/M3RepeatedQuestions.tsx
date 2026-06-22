4c975e2eb80714d47db731636a6f4a36686f079c
shashankmaurya0043-stack <shashankmaurya0043@gmail.com>
2026-06-22 21:14:47 +0530

    Delete artifacts/quiz/src/pages/blog/M3RepeatedQuestions.tsx

--- artifacts/quiz/src/pages/blog/M3RepeatedQuestions.tsx
+++
@@ -1,7 +0,0 @@
-export default function M3RepeatedQuestions() {
-  return (
-    <div className="min-h-screen flex items-center justify-center text-white bg-[#0F172A]">
-      <h1 className="text-2xl font-bold">M4 PYQ 2025 Coming Soon...</h1>
-    </div>
-  );
-}
