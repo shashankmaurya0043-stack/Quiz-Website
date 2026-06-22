7ef87a5c3ee1358619370a0eec47a87444392492
shashankmaurya0043-stack <shashankmaurya0043@gmail.com>
2026-06-22 14:57:00 +0530

    Add HTML quiz questions to M2PYQ2025
    Added multiple HTML-related quiz questions with options and answers.

--- artifacts/quiz/src/pages/blog/M2PYQ2025.tsx
+++ artifacts/quiz/src/pages/blog/M2PYQ2025.tsx
@@ -7,5 +7,106 @@ interface Question {
 }
 
 const questions: Question[] = [
+  {
+    question: "HTML tags are surrounded by which type of brackets? / HTML टैग्स को किन ब्रैकेट्स में लिखा जाता है?",
+    options: [
+      "Angle / कोणीय",
+      "Square / वर्गाकार",
+      "Curly / घुंघराले",
+      "Round / गोलाकार"
+    ],
+    answer: "Angle / कोणीय"
+  },
+  {
+    question: "HTML web pages can be read and rendered by:",
+    options: [
+      "Web Browser / वेब ब्राउज़र",
+      "Interpreter",
+      "Server",
+      "Compiler"
+    ],
+    answer: "Web Browser / वेब ब्राउज़र"
+  },
+  {
+    question: "How to insert a background image in HTML?",
+    options: [
+      "<body background='img.png'>",
+      "<img background='img.png'>",
+      "<bg-image='img.png'>",
+      "None of the above"
+    ],
+    answer: "<body background='img.png'>"
+  },
+  {
+    question: "HTML is what type of language?",
+    options: [
+      "Network Protocol",
+      "Programming Language",
+      "Markup Language",
+      "Scripting Language"
+    ],
+    answer: "Markup Language"
+  },
+  {
+    question: "Tags and texts that are not directly displayed on the page are written in which section?",
+    options: [
+      "<title>",
+      "<html>",
+      "<head>",
+      "<body>"
+    ],
+    answer: "<head>"
+  },
+  {
+    question: "Which HTML tag produces the biggest heading?",
+    options: [
+      "<h4>",
+      "<h>",
+      "<h1>",
+      "<h9>"
+    ],
+    answer: "<h1>"
+  },
+  {
+    question: "Fundamental HTML Block is known as:",
+    options: [
+      "HTML Entity",
+      "HTML Attribute",
+      "HTML Tag",
+      "HTML Body"
+    ],
+    answer: "HTML Tag"
+  },
+  {
+    question: "What tag is used to display a picture in an HTML page?",
+    options: [
+      "<image>",
+      "<picture>",
+      "<img>",
+      "<src>"
+    ],
+    answer: "<img>"
+  },
+  {
+    question: "The page title is inside the ________ tag?",
+    options: [
+      "Table",
+      "Division",
+      "Head",
+      "Body"
+    ],
+    answer: "Head"
+  },
+  {
+    question: "Which tag is used for inline frame?",
+    options: [
+      "<Inframe>",
+      "<iframe>",
+      "<inlineframe>",
+      "<frame>"
+    ],
+    answer: "<iframe>"
+  }
+];
   // यहाँ M2 के Q1-Q100 आएंगे
 ];
